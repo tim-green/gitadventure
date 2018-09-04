f24ca0b3f53a68dae478ea72277cc7db537857d1
Tim <hello@timgreen.ws>
2018-09-04 19:52:33 +1000

    Add files via upload

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+	typography: {
+		fontFamily: '"proxima-nova",sans-serif',
+	},
+	colors: {
+		primaryColor: '#008000',
+		titleColor: 'white'
+	},
+	secondaryColor: '#101010',
+};
+
+export default theme;
